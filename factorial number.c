0f766ef9c044dc735c777efdc0d11e844b3125dc
Naga Vardhan <129207799+192110480@users.noreply.github.com>
2023-03-29 12:52:08 +0530

    Create factorial number.c

---
+++ factorial number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, rem, temp, fact, sum = 0;
+    printf("Enter a number to check strong number: ");
+    scanf("%d", &n);
+    temp = n;
+    while (temp > 0){
+        i = 1, fact = 1;
+        rem = temp % 10;
+        
+        while (i <= rem){
+            fact = fact * i;
+            i++;
+        }
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+    if (n == sum){
+        printf("%d is a strong number.", n);
+    }
+    else {
+        printf("%d is not a strong number.", n);
+    }
+    return 0;
+}
